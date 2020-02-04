6b9a1a9e0f7ad4dc812fa8b93e65b42821bb9e13
MrAlmost1999 <2830114286@qq.com>
2020-02-04 21:08:22 +0800

    修复了不能显示问题描述、访问数无法累加、添加时部分字段为null、分页时产生类型转换异常的bug

--- src/main/java/com/mralmost/community/exception/CustomErrorCode.java
+++ src/main/java/com/mralmost/community/exception/CustomErrorCode.java
@@ -3,7 +3,7 @@ package com.mralmost.community.exception;
 /**
  * @author Lxj
  * @Package com.mralmost.community.exception
- * @Description TODO 错误码
+ * @Description TODO 错误状态码
  * @date: 2020/1/22
  */
 public interface CustomErrorCode {
